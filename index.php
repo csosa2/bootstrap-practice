469c1d2a93538c5dd27d008701886e9b94fad6bc
Christina Sosa <csosa2106@gmail.com>
2016-08-04 16:02:27 -0600

    created grid layout in index.php

---
+++ index.php
@@ -0,0 +1,99 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="UTF-8" />
+		<meta http-equiv="X-UA-Compatible" content="IE=edge"/>
+		<meta name="viewport" content="width=device-width, initial-scale=1" />
+
+		<!-- Latest compiled and minified CSS -->
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+
+		<!-- Optional theme -->
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
+
+		<!-- ADD OTHER CSS FILES HERE -->
+
+		<!-- ADD YOUR CUSTOM CSS HERE -->
+		<link rel="stylesheet" href="stylesheet.css" type="text/css"/>
+
+
+		<!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+		<!--[if lt IE 9]>
+		<script type="text/javascript" src="//oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+		<script type="text/javascript" src="//oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+		<![endif]-->
+
+		<!-- jQuery -->
+		<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/1.9.1/jquery.min.js" type="text/javascript"></script>
+
+		<!-- Latest compiled and minified JavaScript -->
+		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+
+		<!-- ADD YOUR CUSTOM JS FILES HERE -->
+
+
+		<title>My Bootstrap Page</title>
+	</head>
+	<body class="sfooter">
+
+		<!-- begin sticky footer content -->
+		<div class="sfooter-content">
+
+			<!-- begin header -->
+			<header>
+				<div class="container">
+					<div class="row">
+						<div class="col-md-12">
+
+				</div>
+						</div>
+			</header>
+
+			<!-- begin main content -->
+			<main>
+				<div class="container">
+					<div class="row">
+						<div class="col-md-12">
+							<h2>Welcome Message</h2>
+							<p>byline test</p>
+						</div>
+					</div>
+				<div class="container">
+					<div class="row">
+						<div class="col-md-3">
+							<img src="">
+						</div>
+						<div class="col-md-9">
+							content panel
+						</div>
+					</div>
+					<div class="row">
+						<div class="col-md-3">
+							column one
+						</div>
+						<div class="col-md-3">
+							column two
+						</div>
+						<div class="col-md-3">
+							column 3
+						</div>
+						<div class="col-md-3">
+							column 4
+						</div>
+					</div>
+				</div>
+			</main>
+		</div>
+
+		<!-- begin footer here -->
+		<footer>
+			<div class="container">
+				<div class="row">
+					<div class="col-md-12">
+						footer column one
+					</div>
+				</div>
+			</div>
+		</footer>
+	</body>
+</html>
